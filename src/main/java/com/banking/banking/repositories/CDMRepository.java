5afa895ddae782c9f13d7dea0f5cb896c95cc827
Mihir0806 <bhasinmihir8000@gmail.com>
2022-12-05 18:57:24 +0530

    Test cli commit

---
+++ src/main/java/com/banking/banking/repositories/CDMRepository.java
@@ -0,0 +1,2 @@
+package com.banking.banking.repositories;public interface CommonDataModelRepository {
+}
---
+++ src/main/java/com/banking/banking/services/CommonDataModelService.java
@@ -0,0 +1,2 @@
+package com.banking.banking.services;public interface CommonDataModelService {
+}
